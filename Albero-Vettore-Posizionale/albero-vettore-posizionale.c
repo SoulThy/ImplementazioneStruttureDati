2dc184c3fb0e02ce64f6f5d41cd894501b14a2d3
soulthy <daniel.majewski04@gmail.com>
2023-10-25 20:20:45 +0200

    added gitignore

--- Albero-Vettore-Posizionale/albero-vettore-posizionale.c
+++ Albero-Vettore-Posizionale/albero-vettore-posizionale.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World!\n");
+    printf("Hello, World, This is Albero-Vettore-Posizionale!\n");
     return 0;
 }
---
+++ Dizionario/dizionario.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {
+    printf("Hello, World! This is dizionario\n");
+    return 0;
+}
